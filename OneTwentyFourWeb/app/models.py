d5ae6995a348dd7efebdf95c0a479929b4896bf2
Kevin Trieu <dragn194@gmail.com>
2018-04-19 22:48:53 -0400

    Add a model to represent a 2018 riding

--- OneTwentyFourWeb/app/models.py
+++ OneTwentyFourWeb/app/models.py
@@ -4,4 +4,23 @@ Definition of models.
 
 from django.db import models
 
-# Create your models here.
+class Riding(models.Model):
+    
+    riding_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    #LIB data
+    result_lib = models.FloatField()
+    percent_lib = models.FloatField()
+    swing_lib = models.FloatField()
+    #PC data
+    result_pc = models.FloatField()
+    percent_pc = models.FloatField()
+    swing_pc = models.FloatField()
+    #NDP data
+    result_ndp = models.FloatField()
+    percent_ndp = models.FloatField()
+    swing_ndp = models.FloatField()
+    #OTH data
+    result_oth = models.FloatField()
+    percent_oth = models.FloatField()
+    swing_oth = models.FloatField()
