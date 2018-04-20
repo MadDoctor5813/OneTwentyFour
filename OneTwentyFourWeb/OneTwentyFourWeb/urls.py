f283c5df7aa8d5915c4fe388ea5e4da458847a6f
Kevin Trieu <dragn194@gmail.com>
2018-04-19 21:52:15 -0400

    Activate  the admin site

--- OneTwentyFourWeb/OneTwentyFourWeb/urls.py
+++ OneTwentyFourWeb/OneTwentyFourWeb/urls.py
@@ -9,18 +9,13 @@ import django.contrib.auth.views
 import app.forms
 import app.views
 
-# Uncomment the next lines to enable the admin:
-# from django.conf.urls import include
-# from django.contrib import admin
-# admin.autodiscover()
+from django.conf.urls import include
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
     # Examples:
     url(r'^$', app.views.main, name='home'),
-   
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 ]
