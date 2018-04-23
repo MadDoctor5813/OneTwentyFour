d8201bdab4a86d66092f385d1edcd7d85b610d6e
Kevin Trieu <dragn194@gmail.com>
2018-04-23 14:10:32 -0400

    Add api view to get data for a single riding

--- OneTwentyFourWeb/OneTwentyFourWeb/urls.py
+++ OneTwentyFourWeb/OneTwentyFourWeb/urls.py
@@ -16,6 +16,7 @@ admin.autodiscover()
 urlpatterns = [
     # Examples:
     url(r'^$', app.views.main, name='home'),
+    url(r'^api/get_riding_data/(?P<riding_id>\d+)/', app.views.get_riding_data),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
--- OneTwentyFourWeb/app/views.py
+++ OneTwentyFourWeb/app/views.py
@@ -3,9 +3,34 @@ Definition of views.
 """
 
 from django.shortcuts import render
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponseBadRequest, HttpResponse, Http404, JsonResponse
 from django.template import RequestContext
+from django.core.exceptions import ObjectDoesNotExist
 from datetime import datetime
+
+from app.models import Riding, PartyResult
 
 def main(request):
     return render(request, 'app/main.html')
+
+def get_riding_data(request, riding_id=None):
+    if riding_id is None:
+        return HttpResponseBadRequest('Riding id required.')
+    try:
+        riding = Riding.objects.get(riding_id=riding_id)
+    except ObjectDoesNotExist:
+        raise Http404('Riding not found.')
+    response_dict = dict()
+    response_dict['name'] = riding.name
+    response_dict['id'] = riding.riding_id
+    results = dict()
+    percents = dict()
+    swings = dict()
+    for result in riding.results.all():
+        results[result.party] = result.vote_count
+        percents[result.party] = result.percent
+        swings[result.party] = result.swing
+    response_dict['results'] = results
+    response_dict['percents'] = percents
+    response_dict['swings'] = swings
+    return JsonResponse(response_dict)
