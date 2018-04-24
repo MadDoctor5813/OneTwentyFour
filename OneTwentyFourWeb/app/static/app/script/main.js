1d906f65e6e441510c3ba2b87b75ed08c18afdea
Kevin Trieu <dragn194@gmail.com>
2018-04-24 12:33:18 -0400

    Add riding info view

--- OneTwentyFourWeb/app/static/app/script/main.js
+++ OneTwentyFourWeb/app/static/app/script/main.js
@@ -1,6 +1,27 @@
+function updateRidingWindow(id) {
+    idNumeric = id.substring(id.indexOf("_") + 1);
+    $.get("api/get_riding_data/" + idNumeric, function (data) {
+        $("#riding-name").text(data["name"]);
+        for (key in data["percents"]) {
+            selector = "#riding-result-" + key.toLowerCase();
+            $(selector).text(Math.round(data["percents"][key]) + "%");
+        }
+        for (key in data["swings"]) {
+            selector = "#riding-swing-" + key.toLowerCase();
+            $(selector).text(Math.round(data["swings"][key]) + "%");
+        }
+    });
+}
+
 $().ready(function () {
-   //init svg-pan-zoom
-    $("#riding-map")[0].addEventListener("load", function () {
+    svgNode = $("#riding-map")[0];
+    //init svg-pan-zoom
+    svgNode.addEventListener("load", function () {
         svgPanZoom("#riding-map", { maxZoom: 20 })
+        //get the root svg node
+        svgRoot = svgNode.contentDocument.documentElement;
+        $("polygon", svgRoot).click(function (evt) {
+            updateRidingWindow(evt.target.id);
+        });
     });
 })
