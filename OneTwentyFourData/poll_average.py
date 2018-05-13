6f176efc1566228047b35557d1f49792161a4a9e
Kevin Trieu <dragn194@gmail.com>
2018-05-13 00:09:00 -0400

    Add some code to create a polling average and analyze it
    Turns out a moving average is probably good enough.

--- OneTwentyFourData/poll_average.py
+++ OneTwentyFourData/poll_average.py
@@ -1,5 +1,6 @@
 import csv
 import datetime
+import math
 
 FINAL_2014 = {
     'LIB' : 38.65,
@@ -42,8 +43,59 @@ def load_polls(file):
             row_poll.margin_of_error = float(row[7])
             polls.append(row_poll)
     return polls
-            
+    
+#various constants for the weighted average
+
+#the number of polls we take into account for the weighted average
+MAX_POLLS = 6
+
+def weight(poll, poll_idx):
+    if poll_idx <= MAX_POLLS:
+        return 1
+    else:
+        return 0
+
+def weighted_average(polls):
+    weights = dict()
+    weight_sum = 0
+    results = dict()
+    for idx, poll in enumerate(polls):
+        poll_weight = weight(poll, idx)
+        weight_sum += poll_weight
+        weights[poll] = poll_weight
+    for poll in polls:
+        for party, result in poll.results.items():
+            if party not in results:
+                results[party] = 0
+            results[party] += result * weights[poll]
+    for party, result in results.items():
+        results[party] /= weight_sum
+    return results
+
 
 polls_2011 = load_polls('data/2011/op_polls/op_polls.csv')
 polls_2014 = load_polls('data/2014/op_polls/op_polls.csv')
-print(polls_2011)
+
+predicted_2011 = weighted_average(polls_2011)
+predicted_2014 = weighted_average(polls_2014)
+
+print(predicted_2011)
+print(predicted_2014)
+
+errors = list()
+
+for party, result in predicted_2011.items():
+    errors.append(result - FINAL_2011[party])
+
+for party, result in predicted_2014.items():
+    errors.append(result - FINAL_2014[party])
+
+avg_error = sum(errors) / len(errors)
+
+avg_diff = 0
+for error in errors:
+    avg_diff += (error - avg_error) ** 2
+  
+std_dev = math.sqrt(avg_diff / len(errors))
+print(avg_error)
+print(std_dev)
