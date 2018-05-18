5735ced1cdc16b7ca77f7c881a432b060a5e8835
Kevin Trieu <dragn194@gmail.com>
2018-05-17 22:33:53 -0400

    Add polling average code

---
+++ OneTwentyFourWeb/app/management/commands/pollaverage.py
@@ -0,0 +1,30 @@
+from django.core.management.base import BaseCommand
+from app.models import Poll, PollAveragePoint
+import os
+
+POLLS_TO_AVERAGE = 6
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        polls = list(Poll.objects.all().order_by('-date'))
+        PollAveragePoint.objects.all().delete()
+        #generate an average point for each poll
+        for idx in range(len(polls)):
+            #just a simple moving average for now
+            average = dict()
+            polls_used = 0
+            for i in range(POLLS_TO_AVERAGE):
+                try:
+                    poll = polls[idx - i]
+                except IndexError:
+                    #if we go beyond the beginning of the array just break the loop
+                    break
+                for party, result in poll.results.items():
+                    if average.get(party) is None:
+                        average[party] = 0
+                    average[party] += result
+                polls_used += 1
+            for party, result in average.items():
+                average[party] = average[party] / polls_used
+            PollAveragePoint.objects.create(current=average, date=polls[idx].date)
--- OneTwentyFourWeb/app/models.py
+++ OneTwentyFourWeb/app/models.py
@@ -20,5 +20,30 @@ class Riding(models.Model):
 
 admin.site.register(Riding)
 
+class Poll(models.Model):
+    results = pgfields.JSONField(default=None)
+    date = models.DateField()
+
+    class Meta:
+        ordering = ['-date']
+
+    def __str__(self):
+        return f'Poll taken {str(self.date)}'
+
+admin.site.register(Poll)
+
+class PollAveragePoint(models.Model):
+    current = pgfields.JSONField(default=None)
+    date = models.DateField()
+
+    class Meta:
+        ordering = ['-date']
+
+    def __str__(self):
+        return f'Polling average at {str(self.date)}'
+
+
+admin.site.register(PollAveragePoint)
+
 
 
