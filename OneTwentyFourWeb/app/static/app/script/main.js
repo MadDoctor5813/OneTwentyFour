f8a339443fdbbcb86f05e0d7fd6521b9774bde4d
Kevin Trieu <dragn194@gmail.com>
2018-05-18 23:30:44 -0400

    Add riding coloring based on the projected winner

--- OneTwentyFourWeb/app/static/app/script/main.js
+++ OneTwentyFourWeb/app/static/app/script/main.js
@@ -1,15 +1,38 @@
 function updateRidingWindow(id) {
-    idNumeric = id.substring(id.indexOf("_") + 1);
-    riding = ridings[idNumeric]
+    var idNumeric = id.substring(id.indexOf("_") + 1);
+    var riding = ridings[idNumeric]
     $("#riding-name").text(riding["name"]);
-    for (key in riding["projected"]) {
-        selector = "#riding-result-" + key.toLowerCase();
+    for (var key in riding["projected"]) {
+        var selector = "#riding-result-" + key.toLowerCase();
         $(selector).text(Math.round(riding["projected"][key]) + "%");
     }
-    for (key in riding["swings"]) {
-        selector = "#riding-swing-" + key.toLowerCase();
+    for (var key in riding["swings"]) {
+        var selector = "#riding-swing-" + key.toLowerCase();
         $(selector).text(Math.round(riding["swings"][key]) + "%");
     }
+}
+
+//yes, it's already in the CSS but I have no idea how to sync those up
+colors = {
+    'PC': "#2c338e",
+    'LIB': "#ed1b36",
+    'NDP': "#f48120",
+    'OTH': "#98999b"
+}
+
+function colorRiding(idx) {
+    var idNumeric = this.id.substring(this.id.indexOf("_") + 1);
+    var winner = "";
+    var resultMax = 0;
+    var ridingData = ridings[idNumeric];
+    for (var key in ridingData['projected']) {
+        if (ridingData['projected'][key] > resultMax) {
+            resultMax = ridingData['projected'][key];
+            winner = key;
+        }
+    }
+    $(this).css({ 'fill': colors[winner] });
+    console.log(this.resultMax);
 }
 
 $().ready(function () {
@@ -19,8 +42,10 @@ $().ready(function () {
         svgPanZoom("#riding-map", { maxZoom: 20 })
         //get the root svg node
         svgRoot = svgNode.contentDocument.documentElement;
-        $("polygon", svgRoot).click(function (evt) {
+        ridingShapes = $("polygon", svgRoot);
+        ridingShapes.click(function (evt) {
             updateRidingWindow(evt.target.id);
         });
+        ridingShapes.each(colorRiding);
     });
 })
